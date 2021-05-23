3cd1deb099cfed28e2e4863696578377cb7e8a2b
Pablo Palomo <ppalomo@fermator.com>
2021-05-23 19:10:00 +0200

    Deploying to rinkeby

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,4 +1,6 @@
+const path = require("path");
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -22,35 +24,10 @@ task("accounts", "Prints the list of accounts", async () => {
     hardhat: {
       chainId: 31337
     },
-    // mainnet: {
-    //   url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    //   accounts: [`0x${process.env.MAINNET_PRIVATE_KEY}`]
-    // },
-    // goerli: {
-    //   url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    //   accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`]
-    // },
-    // rinkeby: {
-    //   url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    //   accounts: [`0x${process.env.RINKEBY_PRIVATE_KEY}`]
-    // },
-    // ropsten: {
-    //   url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    //   accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY}`],
-    //   gasPrice: 0
-    // },
-    // mumbai: { //80001
-    //   url: "https://rpc-mumbai.maticvigil.com",
-    //   accounts: [`0x${process.env.MUMBAI_PRIVATE_KEY}`]
-    // },
-    // matic: { //137
-    //   url: "https://rpc-mainnet.maticvigil.com",
-    //   accounts: [`0x${process.env.MATIC_PRIVATE_KEY}`]
-    // },
-    // xdai: { //100
-    //   url: "https://rpc-mumbai.maticvigil.com",
-    //   accounts: [`0x${process.env.MATIC_PRIVATE_KEY}`]
-    // }
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_API_KEY}`,
+      accounts: [`0x${process.env.REACT_APP_DEPLOYER_PRIVATE_KEY}`]
+    },
   }
 };
 
