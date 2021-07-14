56f17672066882d0c6d2fc66beb19efc918c5149
ppalomo <ppalomov@gmail.com>
2021-07-14 16:40:29 +0200

    Checking image link

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+![alt text](https://github.com/ppalomo/echion-protocol/blob/master/screenshot01.jpg?raw=true)
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/components/ImportNFT.js
+++ src/components/ImportNFT.js
@@ -28,41 +28,16 @@ import {
     useColorModeValue
   } from '@chakra-ui/react';
 import useStore from '../store';
-import CanvasDraw from "react-canvas-draw";
 import ERC721JSON from '../abis/ERC721.json';
-// import { SketchPicker } from 'react-color';
-// import { HexColorPicker } from "react-colorful";
-import { TwitterPicker  } from 'react-color';
-import { FaUndo, FaBroom, FaFileImage } from 'react-icons/fa';
 import { useContract, useContractByAddress, useAdminContract, useAdminContractByAddress } from '../hooks/contractHooks';
-// import "react-colorful/dist/index.css";
-// import axios from 'axios';
-// import FormData from 'form-data';
-// const pinataSDK = require('@pinata/sdk');
-// const pinata = pinataSDK(process.env.REACT_APP_PINATA_API_KEY, process.env.REACT_APP_PINATA_API_SECRET);
-// const fs = require('fs');
-
-// const IPFS = require('ipfs-api');
-// const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
-
 
 export default function ImportNFT () {
     const [nftAddress, setNFTAddress] = useState("0xE1A19Eb074815e4028768182F8971D222416159A");
     const [nftIndex, setNFTIndex] = useState(0);
     const [ticketPrice, setTicketPrice] = useState(0);
     const [nftImage, setNFTImage] = useState(null);
-
-    // const nftContract = useContract("EchionNFT");
     const factoryContract = useContract("LotteryPoolFactory");
-    // const defaultBgImage = 'images/canvasBackround.jpg';
     const { isWalletConnected, wallet, provider } = useStore();
-    // const saveableCanvas = useRef(null);
-    // const [color, setColor] = useState("#000");
-    // const [bgImage, setBgImage] = useState(defaultBgImage);
-    // const [name, setName] = useState(null)
-    // const colors = ['#000000', '#D9E3F0', '#F47373', '#697689', '#37D67A', '#2CCCE4', '#555555', '#dce775', '#ba68c8', '#FFFFFF'];  
-    // const [hash, setHash] = useState(null);
-    // const [metadataHash, setMetadataHash] = useState(null);
 
     async function handleValidateNFT() {
         const response = await getERC721ImageURL(nftAddress, nftIndex);
@@ -129,122 +104,6 @@ export default function ImportNFT () {
     function handleTicketPriceChange(e) {
         setTicketPrice(e.target.value);
     }
-
-    // function handleSave() {
-    //     let baseCanvas = saveableCanvas.current.canvasContainer.children[3];
-    //     let baseCanvasContex = baseCanvas.getContext('2d');
-    //     baseCanvasContex.drawImage(saveableCanvas.current.canvasContainer.children[1], 0, 0);
-    //     const data = baseCanvas.toDataURL();
-    //     var imageBuffer = decodeBase64Image(data);                
-        
-    //     // Uploading image to IPFS
-    //     ipfs.files.add(imageBuffer.data, (error, result) => {
-    //         if(error) {
-    //             console.error(error)
-    //             return
-    //         }
-    //         setHash(result[0].hash);
-    //         uploadMetadata(result[0].hash);
-    //     });
-    // }
-
-    // async function uploadMetadata(imgHash){
-    //     const metadata = `{
-    //         "image": "https://ipfs.io/ipfs/${imgHash}",
-    //         "name": "${name}",
-    //         "attributes": [
-    //             {
-    //                 "trait_type": "Creator",
-    //                 "value": "${wallet}"
-    //             }
-    //         ]
-    //     }`;
-
-    //     var imageBuffer = Buffer.from(metadata);
-
-    //     // Uploading image to IPFS
-    //     ipfs.files.add(imageBuffer, (error, result) => {
-    //         if(error) {
-    //             console.error(error)
-    //             return
-    //         }
-    //         setMetadataHash(result[0].hash);
-    //         mintNFT(imgHash, result[0].hash);
-    //     });
-    // }
-
-
-    // async function mintNFT(iHash, mHash){
-    //     const imageURI = 'https://ipfs.io/ipfs/' + iHash;
-    //     const metadataURI = 'https://ipfs.io/ipfs/' + mHash;
-    //     try {
-    //         if(nftContract) {
-    //             const tx = await nftContract.mint(imageURI, metadataURI);
-    //             const result = await tx.wait();
-    //             console.log("index = ", result.events[0].args[2].toString());
-    //             console.log("nftContract.address = ", nftContract.address);
-    //             handleCreateLottery(nftContract.address, result.events[0].args[2]);
-    //         }
-    //     } catch (error) {
-            
-    //     }
-    // }
-
-    // async function handleCreateLottery(nftAddress, nftIndex) {
-    //     try {
-    //         if(factoryContract != null) {
-    //             const tx = await factoryContract.createLottery(nftAddress, nftIndex, ethers.utils.parseEther("0.001"), 0, 1);
-    //             await tx.wait();
-    //         }
-    //     } catch (err) {
-    //         console.log("Error: ", err);
-    //     }
-    // }
-
-
-    // function decodeBase64Image(dataString) {
-    //     var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-    //       response = {};
-      
-    //     if (matches.length !== 3) {
-    //       return new Error('Invalid input string');
-    //     }
-      
-    //     response.type = matches[1];
-    //     response.data = new Buffer(matches[2], 'base64');
-      
-    //     return response;
-    // }
-    
-    // function handleChangeBackground(e) {
-    //     var input = document.createElement('input');
-    //     input.type = 'file';
-
-    //     input.onchange = e => { 
-    //         e.preventDefault();
-    //         var file = URL.createObjectURL(e.target.files[0]);
-    //         setBgImage(file);
-    //         saveableCanvas.current.drawImage();
-    //     }
-
-    //     input.click();
-    // }
-
-    // function handleColorChanged(color){
-    //     setColor(color.hex);
-    // }
-
-    // function handleClearCanvas(e) {
-    //     e.preventDefault();
-    //     setBgImage(defaultBgImage);
-    //     saveableCanvas.current.drawImage();
-    //     e.preventDefault();
-    //     saveableCanvas.current.clear();
-    // }
-
-    // function handleNameChange(e) {
-    //     setName(e.target.value);
-    // }
 
     return(
         <VStack
